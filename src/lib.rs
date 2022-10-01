ba232bd30bd641fa5ce710e2f41daa010b8bd4b7
wavelet-noise <ishellstrike@gmail.com>
2022-10-01 03:29:50 +0300

    big noise

--- src/lib.rs
+++ src/lib.rs
@@ -229,9 +229,9 @@ impl State {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
+                            r: 0.0,
+                            g: 0.0,
+                            b: 0.0,
                             a: 1.0,
                         }),
                         store: true,
@@ -272,7 +272,7 @@ pub async fn run() {
         // Winit prevents sizing with CSS, so we have to set
         // the size manually when on web.
         use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450, 400));
+        window.set_inner_size(PhysicalSize::new(1000, 1000));
         
         use winit::platform::web::WindowExtWebSys;
         web_sys::window()
