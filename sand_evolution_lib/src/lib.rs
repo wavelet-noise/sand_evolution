332f77b5ee827cac7a24510dff0b44f6aaa40894
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2023-04-28 16:34:39 +0300

    build fix

--- sand_evolution_lib/src/lib.rs
+++ sand_evolution_lib/src/lib.rs
@@ -3,8 +3,8 @@ pub mod cs;
 pub mod evolution_app;
 pub mod fps_meter;
 pub mod gbuffer;
-pub mod state;
 pub mod update;
+pub mod state;
 
 use ::egui::FontDefinitions;
 use cgmath::num_traits::clamp;
@@ -33,15 +33,6 @@ enum Event {
     RequestRedraw,
 }
 
-/// This is the repaint signal type that egui needs for requesting a repaint from another thread.
-/// It sends the custom RequestRedraw event to the winit event loop.
-struct ExampleRepaintSignal(std::sync::Mutex<winit::event_loop::EventLoopProxy<Event>>);
-
-impl epi::backend::RepaintSignal for ExampleRepaintSignal {
-    fn request_repaint(&self) {
-        self.0.lock().unwrap().send_event(Event::RequestRedraw).ok();
-    }
-}
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
