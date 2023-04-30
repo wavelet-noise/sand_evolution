0341570f59b7528ab2ebb7238b9b0b7007cca740
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2023-04-30 09:43:59 +0300

    Revert "Update push_to_master.yml"
    This reverts commit 0b4910887d1bd6f56ca15e385ba0d45ea8462356.

--- sand_evolution_lib/src/ecs/components/appearance.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Clone, Copy, Component)]
-#[storage(VecStorage)]
-struct Appearance {
-    color: cgmath::Vector3<f32>,
-    origin: cgmath::Vector2<f32>,
-    scale: cgmath::Vector2<f32>,
-    rotation: f32,
-}
--- sand_evolution_lib/src/ecs/systems/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod render_system;
-pub mod instance_update_system;
